691ba5e7c443994e0e8b832bd60d77377c594510
RikSeg <riksegatto@gmail.com>
2023-12-19 10:42:18 -0300

    conclusão do desafio usando I/O e funções

--- Estruturas_Condicionais/desafio.js
+++ Estruturas_Condicionais/desafio.js
@@ -8,6 +8,8 @@ Você terá 3 variáveis. Sendo elas:
 Imprima no console o valor que será gasto para realizar esta viagem.
 */
 
+const { exit } = require('process');
+
 //Interface de I/O:
 const readline = require('readline').createInterface({
     input: process.stdin,
@@ -15,19 +17,32 @@ const readline = require('readline').createInterface({
   });
 //
 
+const vlr_gas = 5.79;
+const vlr_etan = 3.64;
+
 //Formula de cálculo de consumo:
-  function formula(valor_combustivel,eficiencia,distancia){
-    const resultado = (parseFloat(valor_combustivel) * parseFloat(distancia) ) / parseFloat(eficiencia);
-    console.log('O custo da viagem é:',resultado.toFixed(2));
+  function formula(tipo_comb,eficiencia,distancia){
+    if(tipo_comb === "1"){
+      const resultado = (parseFloat(vlr_gas) * parseFloat(distancia) ) / parseFloat(eficiencia);
+      console.log('O custo da viagem é:',resultado.toFixed(2));
+    }else{
+      const resultado = (parseFloat(vlr_etan) * parseFloat(distancia) ) / parseFloat(eficiencia);
+      console.log('O custo da viagem é:',resultado.toFixed(2));
+    }
   }
 //
 
 //função de input:
  function le_parametros(){
-    readline.question('Insira o valor do combustível:',valor_combustivel => {
+    readline.question('Insira o tipo de combustível:\n1-gasolina\n2-etanol\n\n-> ',tipo_comb => {
+      if(tipo_comb != 1 && tipo_comb != 2){
+         console.log('[ERRO]:valor inválido');
+         readline.close();
+         return -1;
+      }
         readline.question('Insira o valor do consumo:',eficiencia => {
             readline.question('Insira a distância:',distancia => {
-                formula(valor_combustivel,eficiencia,distancia);
+                formula(tipo_comb,eficiencia,distancia);
                 
                 readline.close();
             });
