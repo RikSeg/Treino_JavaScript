0b0ea75e3b4072fed59e9954e522b764edeaba71
RikSeg <riksegatto@gmail.com>
2024-01-18 16:04:49 -0300

    continuando aulas pós férias

---
+++ Arrays/aula.js
@@ -0,0 +1,19 @@
+//arrays
+const alunos = ['João','Vítor','Marina'];
+
+console.log(alunos);
+console.log(alunos[0]);
+
+alunos.push('Renan');
+
+alunos[4] = 'Vinícius';
+
+console.log(alunos);
+
+alunos[4] = 'Maria';
+
+console.log(alunos);
+
+//Estruturas de Repetição
+
+
